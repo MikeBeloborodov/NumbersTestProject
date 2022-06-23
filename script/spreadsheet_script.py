06faaac390619b8263c766e3a1bd991f016c9aab
Mikhail Beloborodov <kiritohenjanai@gmail.com>
2022-06-23 10:37:10 +0400

    changed script logic

--- script/spreadsheet_script.py
+++ script/spreadsheet_script.py
@@ -1,6 +1,5 @@
 import os.path
 import requests
-import json
 import math
 import time
 from typing import List
@@ -12,7 +11,6 @@ from google.oauth2 import service_account
 BASE_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 CREDENTIALS_FILE_NAME = "credentials.json"
 CURRENCY_FILE_NAME = "currency.xml"
-OLD_SPREADSHEET_DATA_FILE_NAME = "old_data.json"
 SLEEP_TIME = 3
 
 
@@ -52,6 +50,9 @@ def convert_spreadsheet_data_to_dict(spreadsheet_data: list) -> List[dict]:
     Converts spreadsheet data of a list type into a dict type
     """
     try:
+        save_currency_xml()
+        conv_rate = get_usd_convertion_rate()
+
         converted_data = []
         for counter, value in enumerate(spreadsheet_data):
             if counter == 0:
@@ -62,6 +63,7 @@ def convert_spreadsheet_data_to_dict(spreadsheet_data: list) -> List[dict]:
             order_table_num = None
             order_num = None
             price_usd = None
+            price_rub = None
             delivery_date = None
 
             for item in value:
@@ -78,11 +80,17 @@ def convert_spreadsheet_data_to_dict(spreadsheet_data: list) -> List[dict]:
             if price_usd:
                 if not price_usd.strip().replace(",", "").replace(".", "").isnumeric():
                     price_usd = None
+            
+            # convert usd to rub
+            price_rub = price_usd * conv_rate
+            price_rub = math.trunc(price_rub * 100.0) / 100.0
+
 
             prepared_data = {
                 "order_table_num": order_table_num,
                 "order_num": order_num,
                 "price_usd": price_usd,
+                "price_rub": price_rub,
                 "delivery_date": delivery_date
             }
 
@@ -93,46 +101,6 @@ def convert_spreadsheet_data_to_dict(spreadsheet_data: list) -> List[dict]:
         raise error
     
     return converted_data
-
-
-def load_old_spreadsheet_data() -> List[dict]:
-    """
-    Loads old spreadsheet data to compare with new data
-    """
-    try:
-        path = os.path.join(BASE_DIRECTORY, OLD_SPREADSHEET_DATA_FILE_NAME)
-        with open(path, "r") as file:
-            data_raw = file.read()
-            json_data = json.loads(data_raw)
-
-    except Exception as error:
-        print("Error while loading old spreadsheet data: ")
-        raise error
-
-    return json_data
-
-
-def compare_old_new_data(old_data: List[dict], new_data: List[dict]) -> bool:
-    """
-    Compare new and old spreadsheet data
-    Returns True if they are and False if they're not
-    """
-    try:
-        if not old_data:
-            return True
-
-        if len(new_data) != len(old_data):
-            return True
-        
-        for index in range(len(new_data)):
-            if new_data[index] != old_data[index]:
-                return True
-        
-    except Exception as error:
-        print("Error while comparing old and new data: ")
-        raise error
-
-    return False
 
 
 def save_currency_xml() -> None:
@@ -176,36 +144,6 @@ def get_usd_convertion_rate() -> float:
     return float(usd_rate)
 
 
-def save_old_data_json(spreadsheet_data: List[dict]) -> None:
-    """
-    Prepares and saves all gathered data from the spreadsheet
-    as a json format file.
-    """
-    try:
-        save_currency_xml()
-        conv_rate = get_usd_convertion_rate()
-
-        for data in spreadsheet_data:
-            price_usd = data['price_usd']
-            if price_usd == None:
-                price_rub = None
-            elif not price_usd.replace(".", "").strip().isnumeric():
-                price_rub = None
-            else:
-                price_rub = float(data['price_usd']) * conv_rate
-                price_rub = math.trunc(price_rub * 100.0) / 100.0
-
-            data.update({"price_rub": price_rub})
-
-        path = os.path.join(BASE_DIRECTORY, OLD_SPREADSHEET_DATA_FILE_NAME)
-        with open(path, "w") as file:
-            file.write(json.dumps(spreadsheet_data))
-        
-    except Exception as error:
-        print("Error while saving gathered data as json: ")
-        raise error
-
-
 def send_data_to_server(spreadsheet_data: List[dict]) -> None:
     """
     Sends data to the backend Flask API
@@ -241,32 +179,17 @@ def delete_old_orders():
 
 def main() -> None:
     """
-    Every SLEEP_TIME seconds compares old and new data
-    from the google spreadsheet, if data is different
-    deletes old data from SQL database and rewrites with the new data
+    Every SLEEP_TIME seconds gets data from the google spreadsheet
+    deletes old data from SQL database and rewrites it with the new data
     """
     print("Script is running...")
     while True:
         try:
-            # Get data from the spreadsheet and convert it
             new_data = get_spreadsheet_data()
             new_data = convert_spreadsheet_data_to_dict(new_data)
 
-            # Get old data from the file (if exists) and convert it
-            if os.path.exists(os.path.join(BASE_DIRECTORY, OLD_SPREADSHEET_DATA_FILE_NAME)):
-                old_data = load_old_spreadsheet_data()
-            else:
-                old_data = []
-
-            # Compare old and new data
-            is_different = compare_old_new_data(old_data, new_data)
-
-            # If different saves old data to json, deletes data 
-            # from SQL database and sends new data to the server
-            if is_different:
-                save_old_data_json(new_data)
-                delete_old_orders()
-                send_data_to_server(new_data)
+            delete_old_orders()
+            send_data_to_server(new_data)
 
             time.sleep(SLEEP_TIME)
 
